ace61fd52efec66199420db6689f9e698241bba0
Roman <83417755+romantut1988@users.noreply.github.com>
2023-03-09 02:14:07 +0300

    lesson 1

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -55,6 +55,29 @@ const users = [
         isStudent: true
     },
 ]
+
+const users2 = [
+    {
+        id: 1,
+        name: "Bob",
+        isStudent: true
+    },
+    {
+        id: 2,
+        name: "Alex",
+        isStudent: true
+    },
+    {
+        id: 3,
+        name: "Ann",
+        isStudent: true
+    },
+    {
+        id: 4,
+        name: "Donald",
+        isStudent: true
+    },
+]
 users.shift()
 
 // делаем копию = вносим изменения в копию = используем копию с изменениями
@@ -64,3 +87,32 @@ usersCopy.pop()
 console.log(users)
 console.log(usersCopy)
 console.log(users === usersCopy)
+
+// C(R)UD
+const newUser = {
+    id: 5,
+    name: "John",
+    isStudent: true,
+    isMarried: true
+}
+
+const addUsers = [...users, newUser]
+
+const concatUsers = [...users, ...users2]
+
+// const updUsers = addUsers.map(user => {
+//     if (user.name === 'John') {
+//         return {...user, isStudent: false}
+//     } else {
+//         return user
+//     }
+// })
+const upd1Users = addUsers.map(user => user.name === 'John' ? {...user, isStudent: false} : user)
+console.log(upd1Users)
+
+const upd2Users = upd1Users.map(user => ({...user, isMarried: true}))
+console.log(upd2Users)
+
+// delete
+const deleteUsers = upd2Users.filter(user => user.id !== 5)
+console.log(deleteUsers)
