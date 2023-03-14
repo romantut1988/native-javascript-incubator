48b80c9446cdf32306a9b2d01d3fb151664a7808
Roman <83417755+romantut1988@users.noreply.github.com>
2023-03-14 21:37:55 +0300

    add lesson 2

--- lesson-2/main.js
+++ lesson-2/main.js
@@ -45,6 +45,16 @@ const getScores = (array) => {
     return result
 }
 
+const getScores2 = (array) => {
+    const result = []
+    const getStScores = st => st.scores
+    for (let i = 0; i < array.length; i++) {
+        const newValue = getStScores(array[i])
+        result[i] = newValue
+    }
+    return result
+}
+
 const sMap = (array, func) => {
     const result = []
     for (let i = 0; i < array.length; i++) {
@@ -55,3 +65,21 @@ const sMap = (array, func) => {
 }
 console.log(students.map(getName))
 console.log(sMap(students, getName))
+
+const sFilter = (array, func) => {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i]) === true) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+console.log(sFilter(students, st => st.scores >= 100))
+console.log(students.filter(st => st.scores >= 100))
+
+const sPop = (array) => {
+    const lastEl = array[array.length - 1]
+    array.length = array.length - 1
+    return lastEl
+}
