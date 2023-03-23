1822ea2f4af5a229ef7416317c02ab63dd775973
Roman <83417755+romantut1988@users.noreply.github.com>
2023-03-23 23:44:54 +0300

    start repeat 4 lessons

--- lesson-4/main-04.js
+++ lesson-4/main-04.js
@@ -1,94 +1,29 @@
-const todolistID_1 = "12hf-45jf";
-const todolistID_2 = "34sd-00lq";
+const todoListId_1 = "23hf-76jh"
+const todoListId_2 = "98kh-md09"
 
-const todolists = [
+const todoLists = [
     {
-        id: todolistID_1,
+        id: todoListId_1,
         title: "What to learn",
         filter: "all",
-        // tasks: [
-        //     {id: 12, title: "JS", isDone: false},
-        //     {id: 13, title: "TS", isDone: false},
-        //     {id: 14, title: "HTML", isDone: false},
-        // ]
     },
     {
-        id: todolistID_2,
+        id: todoListId_2,
         title: "What to buy",
         filter: "all",
-        // tasks: [
-        //     {id: 22, title: "Beer", isDone: false},
-        //     {id: 23, title: "Meat", isDone: false},
-        //     {id: 24, title: "Fish", isDone: false},
-        // ]
-    },
+    }
 ]
 
 const tasks = {
-    [todolistID_1]: [
-        {id: 12, title: "JS", isDone: false},
-        {id: 13, title: "TS", isDone: false},
-        {id: 14, title: "HTML", isDone: false},
+    [todoListId_1]: [
+        {id: 1, title: "HTML", isDone: false},
+        {id: 2, title: "CSS", isDone: false},
+        {id: 3, title: "JS/TS", isDone: false},
     ],
-    [todolistID_2]: [
-        {id: 22, title: "Beer", isDone: false},
-        {id: 23, title: "Meat", isDone: false},
-        {id: 24, title: "Fish", isDone: false},
+    [todoListId_2]: [
+        {id: 4, title: "Bread", isDone: false},
+        {id: 5, title: "Milk", isDone: false},
+        {id: 6, title: "Chicken", isDone: false},
     ]
 }
-
-console.log(tasks[todolistID_1].find(t => t.id === 12).title);
-console.log([...tasks[todolistID_2], {id: 25, title: "Water", isDone: false}]);
-console.log(tasks[todolistID_1].map(t => t.title));
-
-const nums = [10, 20, 30] //
-console.log(nums.reduce((acc, el) => acc + el, 0));
-console.log(nums.reduce((acc, el) => acc > el ? acc : el));
-
-let students = [
-    {
-        id: 1,
-        name: "Bob",
-        age: 22,
-        isMarried: true,
-        scores: 85
-    },
-    {
-        id: 2,
-        name: "Alex",
-        age: 21,
-        isMarried: true,
-        scores: 88
-    },
-    {
-        id: 3,
-        name: "Nick",
-        age: 20,
-        isMarried: false,
-        scores: 120
-    },
-    {
-        id: 4,
-        name: "John",
-        age: 23,
-        isMarried: false,
-        scores: 100
-    },
-];
-
-const sts = {
-    "1": {
-        name: "Bob",
-        age: 22,
-        isMarried: true,
-        scores: 85
-    },
-}
-
-console.log(students.reduce((acc, el) => {
-    acc[el.id] = {...el};
-    delete acc[el.id].id;
-    return acc
-}, {}))
-
-console.log(students.reduce((acc, el) => acc + el.age, 0))
+console.log(tasks[todoLists[0].id])
