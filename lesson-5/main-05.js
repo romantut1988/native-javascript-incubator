229049e99fcfa23d17e7577da77e255b7d1f7552
Roman <romantut1988@mail.ru>
2023-04-06 22:28:36 +0300

    parth 5

--- lesson-5/main-05.js
+++ lesson-5/main-05.js
@@ -1,20 +1,36 @@
-const names = ["Artur", "Bob", "Vlad", "Alex", "Donald", "007", "alex", "юрий"]
-console.log(names.sort());
+const name1 = ["Bob", "Alex", "Donald"]
+console.log(name1.sort())
+//1. сортирует строки "из коробки", т.е. без доп. параметров
 
-const numbers = [100, 99, 665, 1000, 2]
-// return > 0 => перестановка
-// return <= 0 => перестановки не будет
+const name2 = ["Donald", "Alex", "aLex", "alex", "игорь", "Юрий"]
+console.log(name2.sort())
+//2. сортирует строки типа по "алфавиту" (unicode)
 
+console.log(name1)
+//3. работает мутабельно (сортирует массив на месте)
 
-const compFn = (a, b) => { // по возрастанию
-    if (a > b) {
-        return 10
-    } else {
-        return -10
-    }
-}
-numbers.sort((a, b) => b - a)
-console.log(numbers);
+console.log(name1.sort() === name1) //true
+//4. возвращает ссылку на исходный массив
+
+const numbers = [1000, 4, 22, 6780, -1]
+console.log(numbers.sort())
+// 5. Для остальных случаев необъодимо передать функцию сравнения (callback)
+
+// const compareFunction = (a, b) => { // по возрастанию
+//     // > 0 - надо переставить
+//     // <= 0 - порядок не меняем
+//     if (a > b) {
+//         return 999
+//     } else {
+//         return -10
+//     }
+// }
+const compareFunction = (a, b) => a - b
+console.log(numbers.sort(compareFunction))
+//6. Функция сравнения должна возвращать число большее или меньшее 0
+
+console.log(numbers.reverse())
+//7. Вместе с sort часто используется revers
 
 let students = [
     {
@@ -55,23 +71,35 @@ let students = [
     },
 ];
 
-console.log(students.sort((a, b) => b.scores - a.scores))
-console.log(students.sort((a, b) => a.scores - b.scores).reverse())
-console.log(students.sort((a, b) => a.name.localeCompare(b.name)))
-console.log(students.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1))
+const sortByName = (a, b) => {
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1
+    } else
+        return -1
+}
 
-const nums = [13, 45, 67, 12, 99, 87]
-// по возрастанию
+console.log(students.sort(sortByName))
+console.log(students.sort((a, b) => a.name.localeCompare(b.name)))
+// 7. Сортировка массива объектов по строковым значениям
+
+console.log(students.sort((a, b) => a.age - b.age))
+//8. Сортировка массива объекта по числовым значениям
+
+// bubble sort
+
+const nums = [23, 45, 12, 11, 99, 34] // n - 1
 
 for (let j = 0; j < nums.length - 1; j++) {
     let isSorted = true
     for (let i = 0; i < nums.length - 1 - j; i++) {
-        isSorted = false;
         if (nums[i] > nums[i + 1]) {
+            isSorted = false;
+            // let temp = nums[i]
+            // nums[i] = nums[i + 1]
+            // nums[i + 1] = temp
             [nums[i + 1], nums[i]] = [nums[i], nums[i + 1]]
         }
     }
     if (isSorted) break
 }
-
-console.log(nums);
+console.log(nums)
