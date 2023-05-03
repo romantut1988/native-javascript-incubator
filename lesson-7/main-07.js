be2cc7c7e79490f9518b43d3f1179d48f0cf260b
Roman <romantut1988@mail.ru>
2023-05-03 20:43:39 +0300

    closure, recursion, local scope

--- lesson-7/main-07.js
+++ lesson-7/main-07.js
@@ -110,4 +110,76 @@ pow()
 //Продолжение темы.
 // Замыкание, лексическое окружение, рекурсия
 //globalLE {} --> null
-const a = 3
+const car = "bmw"
+
+function startEngine() {
+    console.log(`Star ${car}`)
+}
+
+startEngine();
+
+const car1 = "bmw" //globalLE {car: "bmw"} --> null
+const startEngine1 = function () {
+    //startEngine1 {car1" "audi"}
+    //[[environment]] --> globalLE
+    const car1 = "audi";
+    console.log(`Star ${car1}`)
+}
+startEngine1();
+
+const car2 = "bmw" //globalLE {startEngine2: Function, "bmw"} --> null
+function startEngine2() {
+    //[[environment]] --> globalLE
+    const car2 = "audi";
+    return () => {
+        console.log(`Star ${car2}`)
+    };
+}
+
+const foo = startEngine2();
+foo();
+
+const counter = () => {
+    let i = 0;
+    return () => {
+        console.log(++i);
+    };
+};
+const count = counter();
+count();
+count();
+count();
+
+const counter = () => {
+    //[[environment]] --> globalLE
+    //counter1LE {}
+    //counter2LE {}
+    let i = 0;
+    return () => {
+        //[[environment]] --> counter1LE
+        //[[environment]] --> counter2LE
+        console.log(++i);
+    };
+};
+const count1 = counter();
+// globalLE {counter: Function, count1:  Function} --> null
+const count2 = counter();
+// globalLE {counter: Function, count1:  Function} --> null
+count1();//1
+count1();//2
+count1();//3
+
+count2();//1
+count2();//2
+count2();//3
+
+//globalLE {pow: Function} --> null
+const pow = (x, n) => {
+    //powLE {x: 2, n: 3}
+    if(n===1) {
+        return x;
+    } else {
+        return x * pow(x, n - 1);
+    }
+}
+console.log(pow(2, 3)); //8
